4075a8a72682cd539f1f312351099b6f696e5384
Nikolay <ordi-n-x3@yandex.ru>
2026-09-07 20:08:05 +0500

    Скорректировал структуру файлов в ui

--- app/src/main/java/ru/practicum/android/diploma/ui/FiltersFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/FiltersFragment.kt
@@ -1,4 +1,4 @@
-package ru.practicum.android.diploma.ui.root
+package ru.practicum.android.diploma.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [TeamFragment.newInstance] factory method to
+ * Use the [FiltersFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class TeamFragment : Fragment() {
+class FiltersFragment : Fragment() {
     // Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -36,7 +36,7 @@ class TeamFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team, container, false)
+        return inflater.inflate(R.layout.fragment_filters, container, false)
     }
 
     companion object {
@@ -46,12 +46,12 @@ class TeamFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment TeamFragment.
+         * @return A new instance of fragment FiltersFragment.
          */
         // Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            TeamFragment().apply {
+            FiltersFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
