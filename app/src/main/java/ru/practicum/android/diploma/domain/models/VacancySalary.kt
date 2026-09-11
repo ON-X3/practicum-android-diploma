929f9f612ff706ca55e4ec679a79cf4335321e53
Nikolay <ordi-n-x3@yandex.ru>
2026-09-11 13:07:49 +0500

    Скорректировал код по предупреждениям предыдущего коммита

--- app/src/main/java/ru/practicum/android/diploma/domain/models/VacancySalary.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/models/VacancySalary.kt
@@ -1,8 +1,8 @@
 package ru.practicum.android.diploma.domain.models
 
 data class VacancySalary(
-    val from: Integer?,
-    val to: Integer,
+    val from: Int?,
+    val to: Int?,
     val currency: String?
 )
 
--- app/src/main/java/ru/practicum/android/diploma/domain/util/Resource.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/util/Resource.kt
@@ -5,9 +5,9 @@ sealed class Resource<T>(val data: T? = null, val errorCode: ErrorCode? = null) 
     class Error<T>(errorCode: ErrorCode) : Resource<T>(errorCode = errorCode)
 }
 
-enum class ErrorCode(val code: Int) {
-    INTERNAL_SERVER_ERROR(500),
-    NOT_FOUND(404),
-    NO_INTERNET_CONNECTION(-1),
-    LOCAL_DB_ERROR(1)
+enum class ErrorCode {
+    INTERNAL_SERVER_ERROR,
+    NOT_FOUND,
+    NO_INTERNET_CONNECTION,
+    LOCAL_DB_ERROR
 }
