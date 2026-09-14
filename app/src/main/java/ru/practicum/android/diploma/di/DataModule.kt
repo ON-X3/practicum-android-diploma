7202859a9178605f3317046e74a188d3d7c8186c
FencingRex <phenix3legio@gmail.com>
2026-09-14 13:27:04 +0300

    правки

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,5 +1,23 @@
 package ru.practicum.android.diploma.di
 
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.data.network.NetworkClient
+import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
+import ru.practicum.android.diploma.data.network.SearchApi
 
-val dataModule = module { }
+val dataModule = module {
+    single<NetworkClient> {
+        RetrofitNetworkClient(searchApi = get(),androidContext())
+    }
+    single<SearchApi> {
+        Retrofit.Builder()
+            .baseUrl("https://android-diploma.education-services.ru/")
+            .addConverterFactory(GsonConverterFactory.create())
+
+            .build()
+            .create(SearchApi::class.java)
+    }
+}
