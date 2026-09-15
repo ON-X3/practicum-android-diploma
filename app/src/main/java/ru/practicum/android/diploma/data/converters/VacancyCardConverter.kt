41d4ec4f75c862034526e179b814f6ff550c1735
Van13hhh <novikovivan06@gmail.com>
2026-09-15 21:14:16 +0300

    поправил замечания детекта

--- app/src/main/java/ru/practicum/android/diploma/data/converters/VacancyCardConverter.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/converters/VacancyCardConverter.kt
@@ -5,22 +5,26 @@ import ru.practicum.android.diploma.domain.models.VacancyCard
 import ru.practicum.android.diploma.domain.models.VacancySalary
 
 class VacancyCardConverter {
-    fun toVacancyCard(entity: VacancyDetailEntity): VacancyCard {
-        return VacancyCard(
-                id = entity.id,
-                name = entity.name,
-                company = entity.employerName,
-                city = entity.area,
-                salary = if (entity.salaryFrom != null || entity.salaryTo != null || entity.salaryCurrency != null) {
-                    VacancySalary(
-                        from = entity.salaryFrom,
-                        to = entity.salaryTo,
-                        currency = entity.salaryCurrency,
-                    )
-                } else {
-                    null
-                },
-                logo = entity.employerLogo
-            )
-        }
+
+    fun toVacancyCard(entity: VacancyDetailEntity): VacancyCard =
+        VacancyCard(
+            id = entity.id,
+            name = entity.name,
+            company = entity.employerName,
+            city = entity.area,
+            salary = if (
+                entity.salaryFrom != null ||
+                entity.salaryTo != null ||
+                entity.salaryCurrency != null
+            ) {
+                VacancySalary(
+                    from = entity.salaryFrom,
+                    to = entity.salaryTo,
+                    currency = entity.salaryCurrency,
+                )
+            } else {
+                null
+            },
+            logo = entity.employerLogo,
+        )
 }
