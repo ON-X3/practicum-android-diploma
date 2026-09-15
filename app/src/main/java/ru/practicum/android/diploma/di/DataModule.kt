f38c02770b32e442539a52d0a753ca9fc15d390f
FencingRex <phenix3legio@gmail.com>
2026-09-15 14:20:14 +0300

    detekt

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,6 +1,5 @@
 package ru.practicum.android.diploma.di
 
-import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
@@ -20,30 +19,4 @@ val dataModule = module {
             .build()
             .create(SearchApi::class.java)
     }
-}
-import ru.practicum.android.diploma.BuildConfig
-import ru.practicum.android.diploma.data.converters.VacancyDomainConverter
-import ru.practicum.android.diploma.data.network.AuthInterceptor
-import ru.practicum.android.diploma.data.network.NetworkClient
-import ru.practicum.android.diploma.data.network.RetrofitNetworkClient
-import ru.practicum.android.diploma.data.network.SearchApi
-import ru.practicum.android.diploma.data.repository.SearchRepositoryImpl
-import ru.practicum.android.diploma.domain.api.SearchRepository
-
-val dataModule = module {
-    single<SearchApi> {
-        val client = OkHttpClient.Builder()
-            .addInterceptor(AuthInterceptor(BuildConfig.API_ACCESS_TOKEN))
-            .build()
-
-        Retrofit.Builder()
-            .baseUrl("https://android-diploma.education-services.ru/")
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-            .create(SearchApi::class.java)
-    }
-    single<NetworkClient> { RetrofitNetworkClient(get(), androidContext()) }
-    single { VacancyDomainConverter() }
-    single<SearchRepository> { SearchRepositoryImpl(get(), get()) }
 }
