daa24001c584fcd4930124ed398b10ab88078255
tuca <pectopa4pda@yandex.ru>
2026-09-17 21:47:17 +0300

    E2T5 - обработка ошибок при получении вакансии

--- app/src/main/java/ru/practicum/android/diploma/di/DomainModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DomainModule.kt
@@ -2,8 +2,11 @@ package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
 import ru.practicum.android.diploma.domain.api.SearchInteractor
+import ru.practicum.android.diploma.domain.api.VacancyInteractor
 import ru.practicum.android.diploma.domain.impl.SearchInteractorImpl
+import ru.practicum.android.diploma.domain.impl.VacancyInteractorImpl
 
 val domainModule = module {
     single<SearchInteractor> { SearchInteractorImpl(get()) }
+    single<VacancyInteractor> { VacancyInteractorImpl(repository = get()) }
 }
--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -1,5 +1,9 @@
 package ru.practicum.android.diploma.di
 
+import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import ru.practicum.android.diploma.ui.VacancyViewModel
 
-val viewModelModule = module { }
+val viewModelModule = module {
+    viewModel { VacancyViewModel(repository = get()) }
+}
