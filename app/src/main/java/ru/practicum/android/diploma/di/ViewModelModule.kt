3bca993ad27bb9ddb47c0dd1193eb22d0fc16a59
tuca <pectopa4pda@yandex.ru>
2026-09-18 19:17:37 +0300

    E3T2 - Создание viewModel для экрана избранного

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -3,9 +3,13 @@ package ru.practicum.android.diploma.di
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.SearchViewModel
+import ru.practicum.android.diploma.ui.FavoritesViewModel
 
 val viewModelModule = module {
     viewModel {
         SearchViewModel(get())
+    }
+    viewModel {
+        FavoritesViewModel(favoritesInteractor = get())
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/FavoritesFragment.kt
@@ -9,9 +9,12 @@ import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFavoritesBinding
 import ru.practicum.android.diploma.domain.models.VacancyCard
+import androidx.core.view.isVisible
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FavoritesFragment : Fragment() {
 
+    private val viewModel: FavoritesViewModel by viewModel()
     private var _adapter: VacancyCardAdapter? = null
     private val adapter get() = _adapter!!
 
@@ -33,7 +36,33 @@ class FavoritesFragment : Fragment() {
             onVacancyCardClick(vacancyCard)
         }
         binding.favoritesList.adapter = adapter
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            render(state)
+        }
+        viewModel.getFavorites()
+    }
 
+    override fun onResume() {
+        super.onResume()
+        viewModel.getFavorites()
+    }
+
+    private fun render(state: FavoritesScreenState) {
+        when (state) {
+            is FavoritesScreenState.Loading -> {
+                binding.favoritesList.isVisible = false
+            }
+            is FavoritesScreenState.Empty -> {
+                binding.favoritesList.isVisible = false
+            }
+            is FavoritesScreenState.Content -> {
+                binding.favoritesList.isVisible = true
+                addVacanciesToList(state.vacancies)
+            }
+            is FavoritesScreenState.Error -> {
+                binding.favoritesList.isVisible = false
+            }
+        }
     }
 
     override fun onDestroyView() {
@@ -43,8 +72,8 @@ class FavoritesFragment : Fragment() {
         super.onDestroyView()
     }
 
-    private fun addVacanciesToList(vacancies: List<VacancyCard>, hasNextPage: Boolean) {
-        adapter.addVacancies(vacancies, hasNextPage)
+    private fun addVacanciesToList(vacancies: List<VacancyCard>) {
+        adapter.updateVacancies(vacancies)
     }
 
     private fun onVacancyCardClick(vacancyCard: VacancyCard) {
