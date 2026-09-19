97a899b4495c5006a670a31da8e2535ab34e5b0c
tuca <pectopa4pda@yandex.ru>
2026-09-19 15:34:26 +0300

    E3T2

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -2,6 +2,10 @@ package ru.practicum.android.diploma.di
 
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import ru.practicum.android.diploma.domain.api.FavoritesInteractor
+import ru.practicum.android.diploma.domain.models.FavoriteVacancy
+import ru.practicum.android.diploma.domain.models.VacancyCard
+import ru.practicum.android.diploma.domain.util.Resource
 import ru.practicum.android.diploma.presentation.SearchViewModel
 import ru.practicum.android.diploma.ui.FavoritesViewModel
 
@@ -10,6 +14,28 @@ val viewModelModule = module {
         SearchViewModel(get())
     }
     viewModel {
-        FavoritesViewModel(favoritesInteractor = get())
+        FavoritesViewModel(
+            favoritesInteractor = object : FavoritesInteractor {
+                override suspend fun addToFavorite(vacancy: FavoriteVacancy): Resource<Unit> {
+                    return Resource.Success(Unit)
+                }
+
+                override suspend fun deleteFromFavoriteById(id: String): Resource<Unit> {
+                    return Resource.Success(Unit)
+                }
+
+                override suspend fun isFavorite(id: String): Resource<Boolean> {
+                    return Resource.Success(false)
+                }
+
+                override suspend fun getVacancyById(id: String): Resource<FavoriteVacancy> {
+                    throw NotImplementedError("TODO")
+                }
+
+                override suspend fun getFavoriteList(): Resource<List<VacancyCard>> {
+                    return Resource.Success(emptyList())
+                }
+            }
+        )
     }
 }
