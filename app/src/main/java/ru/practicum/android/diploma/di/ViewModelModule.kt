dc7d2250f65e4131bd0a700134b53f4701cf13ee
Nikolay <ordi-n-x3@yandex.ru>
2026-09-20 20:56:43 +0500

    Перенес FavoritesViewModel и FavoritesScreenState в presentation Добавил прогресс бар загрузки

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -4,7 +4,7 @@ import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
 import ru.practicum.android.diploma.presentation.SearchViewModel
 import ru.practicum.android.diploma.presentation.VacancyDetailViewModel
-import ru.practicum.android.diploma.ui.FavoritesViewModel
+import ru.practicum.android.diploma.presentation.FavoritesViewModel
 
 val viewModelModule = module {
     viewModel {
