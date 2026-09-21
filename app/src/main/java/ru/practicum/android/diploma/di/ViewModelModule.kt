44ae6517ce4cde14293a5a3309d79b4d3ab8b111
Nikolay <ordi-n-x3@yandex.ru>
2026-09-21 10:50:11 +0500

    Объявил IndustryViewModel в коин, чтобы приложение не падало при попытке перейти на экран выбора отрасли

--- app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/ViewModelModule.kt
@@ -2,6 +2,7 @@ package ru.practicum.android.diploma.di
 
 import org.koin.core.module.dsl.viewModel
 import org.koin.dsl.module
+import ru.practicum.android.diploma.presentation.IndustryViewModel
 import ru.practicum.android.diploma.presentation.SearchViewModel
 import ru.practicum.android.diploma.presentation.VacancyDetailViewModel
 import ru.practicum.android.diploma.ui.FavoritesViewModel
@@ -16,4 +17,5 @@ val viewModelModule = module {
     viewModel {
         FavoritesViewModel(get())
     }
+    viewModel { IndustryViewModel(get()) }
 }
